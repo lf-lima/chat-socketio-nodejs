ea21b2bb9fdcd2868a2a7f090ba94380b8a5af9a
Luiz Felipe Pereira de Lima <luiz.lima>
2022-04-10 17:51:43 -0300

    make chat with others users success

--- index.js
+++ index.js
@@ -5,20 +5,35 @@ const path = require('path')
 
 const app = express()
 const httpServer = createServer(app)
-const io = new Server(httpServer)
+const io = new Server(httpServer, { cors: { origin: '*' } })
+
+const messages = []
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, './index.html'))
 })
 
-io.on('connection', client => {
-  console.log('Test connection')
+io.on('connection', socket => {
+  socket.emit('message:render-olds', messages)
 
-  io.on('hello', (arg) => {
-    console.log(arg)
+  let username = ''
+  socket.on('user:enter', (_username) => {
+    username = _username
+    console.log(`New user: ${username}`)
+  })
+
+  socket.on('message:send', (message) => {
+    const messageData = {
+      username,
+      message
+    }
+
+    messages.push(messageData)
+
+    socket.broadcast.emit('message:new', messageData)
   })
 })
 
 httpServer.listen(3333, () => {
-  console.log('App listen port 3333')
+  console.log('App listen port http://localhost:3333')
 })
